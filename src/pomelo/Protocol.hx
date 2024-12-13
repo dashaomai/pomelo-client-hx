1dbbfa1d6d8b3998ecb0239a121cadf3c8333840
dashaomai <dashaomai@gmail.com>
2024-12-13 16:16:52 +0800

    feat: Support the compress message from server.

--- src/pomelo/Protocol.hx
+++ src/pomelo/Protocol.hx
@@ -20,6 +20,49 @@ class Protocol {
     }
 
     static public function str_decode(buffer: Bytes): String {
+        final type = is_compressed(buffer);
+        if (type != Uncompress) {
+            buffer = inflate_data(buffer, type);
+        }
+
         return buffer.toString();
+    }
+}
+
+enum CompressType {
+    Uncompress;
+    ZLib;
+    GZip;
+}
+
+// support with pitaya message compress
+function is_compressed(data: Bytes): CompressType {
+    if (data == null || data.length < 2) {
+        return Uncompress;
+    }
+
+    final d0 = data.get(0);
+    final d1 = data.get(1);
+
+    if (d0 == 0x78 && (
+        d1 == 0x9C ||
+        d1 == 0x01 ||
+        d1 == 0xDA ||
+        d1 == 0x5E
+    )) {
+        return ZLib;
+    } else if (d0 == 0x1F && d1 == 0x8B) {
+        return GZip;
+    } else {
+        return Uncompress;
+    }
+}
+
+function inflate_data(data: Bytes, type: CompressType): Bytes {
+    switch (type) {
+        case ZLib | GZip:
+            return haxe.zip.Uncompress.run(data);
+        default:
+            return data;    
     }
 }
